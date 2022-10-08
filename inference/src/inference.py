e5c7bcc50cd5697894725059f33290499143aeac
= <=>
2022-10-07 23:08:44 -0500

    actualizacion repo

---
+++ inference/src/inference.py
@@ -0,0 +1,74 @@
+import csv
+import numpy as np
+import cv2
+
+import grpc
+
+import tensorflow as tf
+from tensorflow.keras import models
+from concurrent import futures
+
+import inference_pb2
+import inference_pb2_grpc
+
+
+
+class InferenceService(inference_pb2_grpc.InferenceServicer):
+    def _read_img(self, path):
+        img = cv2.imread(path)
+        img_array = np.asarray(img)
+        img2 = img.array.astype(float)
+        img2 = (np.maximum(img2,0) / img2.max()) * 255.0
+        img2 = np.unit8(img2)
+
+        return img2
+
+    def predict (self, request, context):
+        #load image
+        path = request.path
+        array = self._read_img(path=path)
+
+        patch_array_img = self.preprocess(array)
+
+        #Up model
+        modelo = self.modelo()
+
+        prediction = np.argmax(modelo.predict(batch_array_img))
+
+        fit_percent = np.max(modelo.predict(batch_array_img))* 100
+        str_dataresult = ''
+        if prediction == 0:
+            str_dataresult = "bacteriana"
+        if prediction == 1:
+            str_dataresult = "normal"
+        if prediction == 2:
+            str_dataresult = "viral"
+
+        reponse_message = inference_pb2.datapred(percent = fit_percent, dataresult = str_dataresult)
+        return reponse_message
+
+    def preprocess(array):
+        array = cv2.resize(array, (512, 512))
+        array = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
+        array = clahe.apply(array)
+        array = array / 255
+        array = np.expand_dims(array, axis=-1)
+        array = np.expand_dims(array, axis=0)
+        return array
+
+    def model(self):
+        model_cnn = tf.keras.models.load_model('WilhemNet_86.h5')
+        return model_cnn
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    inference_pb2_grpc.add_InferenceServicer_to_server(InferenceService(), server)
+    server.add_insecure_port("[::]:50052")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    serve()
+
---
+++ interface/src/interface.py
@@ -0,0 +1,124 @@
+# import the necessary packages
+from curses import panel
+from tkinter import Tk, Button, Label, filedialog, Entry
+from tkinter.messagebox import askokcancel, showinfo, WARNING
+import getpass
+import base64
+
+from turtle import clear
+
+import grpc
+from PIL import Image
+from PIL import ImageTk
+import numpy as np
+
+import backend_pb2
+import backend_pb2_grpc
+
+import inference_pb2
+import inference_pb2_grpc
+
+root = Tk()
+
+panelA = None
+srtPath = None
+
+def run_model():
+    global strPath
+
+    path_msg = inference_pb2.img_path2(path=strPath)
+    response = inference_client.predict(path_msg)
+
+    v_percent = response.percent
+    v_result = response.dataresult
+
+    text2.insert(END, v_result)
+    text3.insert(END, '{:2.f}'.format(v_percent) + '%')
+
+
+def select_image():
+    # grab a reference to the image panels
+    global panelA, backend_client, strPath
+    # open a file chooser dialog and allow the user to select an input
+    # image
+    #path = filedialog.askopenfilename()
+
+    path = filedialog.askopenfilename(
+            title="Select image",
+            filetypes=(
+                ("DICOM", "*.dcm"),
+                ("JPEG", "*.jpeg"),
+                ("jpg files", "*.jpg"),
+                ("png files", "*.png"),
+            )
+    )
+
+    # ensure a file path was selected
+    if len(path) > 0:
+
+        path_message = backend_pb2.img_path(path=path)
+        response = backend_client.load_image(path_message)
+
+        img_content = response.img_content
+        img_w = response.width
+        img_h = response.height
+
+        b64decoded = base64.b64decode(img_content)
+        image = np.frombuffer(b64decoded, dtype=np.uint8).reshape(img_h, img_w, -1)
+
+        # convert the images to PIL format...
+        image = Image.fromarray(image)
+        # ...and then to ImageTk format
+        image = ImageTk.PhotoImage(image)
+
+        # if the panels are None, initialize them
+        if panelA is None:
+            # the first panel will store our original image
+            panelA = Label(image=image)
+            panelA.image = image
+            panelA.pack(side="left", padx=10, pady=10)
+        else:
+            # update the pannels
+            panelA.configure(image=image)
+            panelA.image = image
+
+#Input boxes
+text2 = Text(root)
+text3 = Text(root)
+
+#buttons
+button1 = ttk.Button(root, text='Predecir', state='disable', command=run_model)
+button2 = ttk.Button(root, text='Cargar Imagen', command=select_image)
+
+#widget position
+lab1.place(x=110, y=65)
+lab3.place(x=80, y=370)
+lab6.place(x=80, y=415)
+button1.place(x=220, y=460)
+button2.place(x=70, y=460)
+text2.place(x=220, y=370, width=90, height=30)
+text3.place(x=220, y=415, width=90, height=30)
+
+# initialize the window toolkit along with the two image panels
+root = Tk()
+panelA = None
+
+# Backend client definition
+MAX_MESSAGE_LENGTH = 256*1024*1024
+
+options = [('grpc.max_send_message_length', MAX_MESSAGE_LENGTH), 
+('grpc.max_recive_message_length', MAX_MESSAGE_LENGTH)]
+channel = grpc.insecure_channel("backend:50051", options=options)
+backend_client = backend_pb2_grpc.BackendStub(channel=channel)
+
+#Inference definition
+channel2 = grpc.insecure_channel("backend:50052", options=options)
+infrence_client = inference_pb2_grpc.inferenceStub(channel=channel2)
+
+# create a button, then when pressed, will trigger a file chooser
+# dialog and allow the user to select an input image; then add the
+# button the GUI
+btn = Button(root, text="Select an image", command=select_image)
+btn.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
+root.mainloop()
